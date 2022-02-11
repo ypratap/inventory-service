327749ee8cf80e6d1b4bf88408c2b2d82edcf4e7
pratapy <pratapyalamanchi.1@gmail.com>
2022-02-10 21:43:50 -0500

    Initial commit

---
+++ src/main/java/io/shiprock/inventoryservice/controller/InventoryRestController.java
@@ -0,0 +1,27 @@
+package io.shiprock.inventoryservice.controller;
+
+import io.shiprock.inventoryservice.model.Inventory;
+import io.shiprock.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryRestController {
+
+    private final InventoryRepository inventoryRepository;
+
+    @GetMapping("/{skuCode}")
+    Boolean isInStock(@PathVariable String skuCode) {
+        log.info("Checking stock for product with skucode - " + skuCode);
+        Inventory inventory = inventoryRepository.findBySkuCode(skuCode)
+                .orElseThrow(() -> new RuntimeException("Cannot Find Product by sku code " + skuCode));
+        return inventory.getStock() > 0;
+    }
+}
